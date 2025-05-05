42366d0ea71621b5da9eb753681e9ef34167ef5d
villagerunknown <VillagerUnknown@proton.me>
2025-05-05 13:27:18 -0400

    Changing config's default max coin stack size

--- src/main/java/me/villagerunknown/villagercoin/VillagercoinConfigData.java
+++ src/main/java/me/villagerunknown/villagercoin/VillagercoinConfigData.java
@@ -14,7 +14,7 @@ public class VillagercoinConfigData implements me.shedaniel.autoconfig.ConfigDat
 	public int currencyConversionMultiplier = 100;
 	
 	@ConfigEntry.Category("Coins")
-	public int maximumCoinStackSize = 5000;
+	public int maximumCoinStackSize = 1000;
 	
 	@ConfigEntry.Category("Coins")
 	public boolean enableCoinFlipping = true;
