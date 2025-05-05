b391123fd7caaab98150a143c92c60f35de10064
villagerunknown <VillagerUnknown@proton.me>
2025-05-05 05:06:37 -0400

    Changing Codec.rangedInt to resolve issues with modded containers (#101)
    Changing Codec.rangedInt method to use Integer.MAX_VALUE if the max is 99 or greater

--- src/main/java/me/villagerunknown/villagercoin/mixin/ItemStackMixin.java
+++ src/main/java/me/villagerunknown/villagercoin/mixin/ItemStackMixin.java
@@ -4,27 +4,27 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import me.villagerunknown.villagercoin.Villagercoin;
 import net.minecraft.component.ComponentChanges;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.dynamic.Codecs;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static net.minecraft.item.ItemStack.ITEM_CODEC;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
 	
+	@Mutable
+	@Final
 	@Shadow
-	public static final Codec<ItemStack> CODEC = Codec.lazyInitialized(() -> {
-		return RecordCodecBuilder.create((instance) -> {
-			return instance.group(ITEM_CODEC.fieldOf("id").forGetter(ItemStack::getRegistryEntry), Codecs.rangedInt(1, Villagercoin.MAX_STACK_COUNT).fieldOf("count").orElse(1).forGetter(ItemStack::getCount), ComponentChanges.CODEC.optionalFieldOf("components", ComponentChanges.EMPTY).forGetter((stack) -> {
-				return stack.getComponentChanges();
-			})).apply(instance, ItemStack::new);
-		});
-	});
+	public static final Codec<ItemStack> CODEC;
 	
 	@Inject(method = "capCount", at = @At("HEAD"), cancellable = true)
 	private void capCount(int maxCount, CallbackInfo ci) {
@@ -35,6 +35,16 @@ public abstract class ItemStackMixin {
 		}
 		
 		ci.cancel();
+	}
+	
+	static{
+		CODEC = Codec.lazyInitialized(() -> {
+			return RecordCodecBuilder.create((instance) -> {
+				return instance.group(ITEM_CODEC.fieldOf("id").forGetter(ItemStack::getRegistryEntry), Codecs.rangedInt(1, Integer.MAX_VALUE).fieldOf("count").orElse(1).forGetter(ItemStack::getCount), ComponentChanges.CODEC.optionalFieldOf("components", ComponentChanges.EMPTY).forGetter((stack) -> {
+					return stack.getComponentChanges();
+				})).apply(instance, ItemStack::new);
+			});
+		});
 	}
 	
 }
